5c00b5cc92ad3f803fe3d0e1f6ed1a38efe55fb7
zly5201314 <2874055396@qq.com>
2019-04-12 19:09:05 +0800

    分支提交

--- workspace_jpa/src/main/java/com/jk/controller/page/PageController.java
+++ workspace_jpa/src/main/java/com/jk/controller/page/PageController.java
@@ -22,7 +22,6 @@ public class PageController {
     @RequestMapping("easyui")
     public String easyui(){
         System.out.println("111");
-        System.out.println("222");
         return  "main";
     }
 
