37d55fb8b4703f7903a358b9eb4c25e0569aaa6e
zly5201314 <2874055396@qq.com>
2019-04-12 19:01:59 +0800

    分支提交

--- workspace_jpa/src/main/java/com/jk/controller/page/PageController.java
+++ workspace_jpa/src/main/java/com/jk/controller/page/PageController.java
@@ -29,6 +29,8 @@ public class PageController {
     //商品查询
     @RequestMapping("queryShop")
     public String queryShop(){
+        System.out.println("1111");
+        System.out.println("222");
         return "shop";
     }
 
